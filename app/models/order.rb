dba03464fa265a3069902f28cddfd84f4453839e
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-19 16:00:59 -0700

    Tests associations between order and user

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   has_many :orders_parts
   has_many :parts, through: :orders_parts
+  belongs_to :orderer, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
   validates :orderer_id, :submitted, presence: true
 end
