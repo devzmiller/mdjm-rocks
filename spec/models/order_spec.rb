60a44073c74eb58f63ea033513d33d449c28c6e7
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-19 15:43:13 -0700

    Added validation tests for order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   describe "validations" do
-    
+    it { should validate_presence_of(:orderer_id) }
+    it { should validate_presence_of(:received_date) }
+    it { should validate_presence_of(:receiver_id) }
+    it { should validate_presence_of(:submitted) }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,7 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
 
     # Choose one or more libraries:
-    with.library :active_record
+    with.library :rails
   end
 end
 
