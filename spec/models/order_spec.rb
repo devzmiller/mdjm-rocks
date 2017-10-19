ee62d79dcaa61823be44294260637a8cdd5cbd6f
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-19 16:00:59 -0700

    Tests associations between order and user

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  let!(:user) {create(:user)}
   describe "validations" do
     it { should validate_presence_of(:orderer_id) }
     it { should validate_presence_of(:submitted) }
@@ -8,5 +9,7 @@ RSpec.describe Order, type: :model do
   describe "associations" do
     it { should have_many(:orders_parts) }
     it { should have_many(:parts) }
+    it { should belong_to(:receiver) }
+    it { should belong_to(:orderer) }
   end
 end
