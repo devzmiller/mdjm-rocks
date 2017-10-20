887e7afdc682236f7c1b599caa4fa1debcc51f63
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-20 08:13:15 -0700

    Mark discrepancies between quantity received and submitted on order show form

--- app/models/orders_part.rb
+++ app/models/orders_part.rb
@@ -3,4 +3,5 @@ class OrdersPart < ApplicationRecord
   belongs_to :order
 
   validates :part, :order, :quantity_ordered, presence: true
+  validates :quantity_ordered, :quantity_received, numericality: { greater_than: 0 }
 end
