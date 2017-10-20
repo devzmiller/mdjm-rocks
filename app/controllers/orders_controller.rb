97c7b0ca0cfde0e558ad1a2c4f92e22f63a74129
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-19 21:18:50 -0700

    Links create new order form to parts#index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,27 @@ class OrdersController < ApplicationController
   def new
     @order_part = OrdersPart.new
   end
+
+  def create
+    puts "****************************************"
+    @part = Part.find_or_initialize_by(part_number: params[:part_num])
+    if @part.name
+      if @part.name == params[:part]
+        @order = Order.new(orderer_id: session[:user_id])
+        @order.parts << @part
+        @order.save!
+        redirect_to order_path(@order)
+      else
+        @errors = ["Part name is invalid"]
+        render :new
+      end
+    else
+      @part.name = params[:part]
+      @part.max_quantity = 100
+      @order = Order.new(orderer_id: session[:user_id], submitted: false)
+      @order.parts << @part
+      @order.save!
+      redirect_to order_path(@order)
+    end
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
-  validates :orderer_id, :submitted, presence: true
+  validates :orderer_id, presence: true
+  validate :submitted_cannot_be_nil
   has_many :orders_parts
   has_many :parts, through: :orders_parts
   belongs_to :orderer, class_name: 'User'
@@ -7,5 +8,11 @@ class Order < ApplicationRecord
 
   def in_shipment?
     submitted
+  end
+
+  def submitted_cannot_be_nil
+    if submitted == nil
+      errors.add(:submitted, "cannot be nil")
+    end
   end
 end
