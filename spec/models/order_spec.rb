1cfed3e0a938aa59eeaf3cf386ab1f8e6e1ecd6c
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-20 08:04:19 -0700

    Test for helper method in Orders model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe Order, type: :model do
       expect(order).to_not be_valid
     end
   end
+  describe "in_shipment?" do
+    it "returns whether or not the order has been submitted" do
+      expect(order.in_shipment?).to eq(order.submitted)
+    end
+  end
   describe "associations" do
     it { should have_many(:orders_parts) }
     it { should have_many(:parts) }
