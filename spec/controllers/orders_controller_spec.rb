ee376e3d37316ed1560b14e89d7cac85fe48417c
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-19 23:25:30 -0700

    Better errors checking for orders#create

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -14,4 +14,47 @@ RSpec.describe OrdersController, type: :controller do
       expect(assigns[:orders]).to eq(Order.all)
     end
   end
+
+  describe "#new" do
+    before(:each) { get :new }
+
+    it "returns 200" do
+      expect(response).to be_ok
+    end
+
+    it "assigns empty Order to @order" do
+      expect(assigns[:order]).to be_a(Order)
+    end
+  end
+
+  describe "#create" do
+    let(:part) { create :part }
+    let(:user) { create :user }
+    context "valid input" do
+      before(:each) { post :create, params: {part_num: part.part_number, part: part.name, quantity: 10},
+                                    session: {user_id: user.id}}
+      it "creates a new order" do
+        expect(assigns[:order]).to eq(Order.last)
+      end
+
+      it "Adds part to order" do
+        order = assigns[:order]
+        expect(Order.find(order.id).parts).to include(part)
+      end
+    end
+    context "invalid input" do
+      it "renders :new template when existing part number doesn't match part name" do
+        post :create, params: {part_num: part.part_number, part: "wenis", quantity: 10},
+                      session: {user_id: user.id}
+
+        expect(response).to render_template(:new)
+      end
+      it "renders :new template when fields are omitted" do
+        post :create, params: {part_num: "", part: "wenis", quantity: 10},
+                      session: {user_id: user.id}
+
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
