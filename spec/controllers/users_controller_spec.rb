fa471316671930127778d9535b511268fd737505
Devin Miller & Miguel Angel Ibarra, Jr <pair+devzmiller.migaibarra@devbootcamp.com>
2017-10-20 12:26:37 -0700

    Write tests for user#create controller route

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   let!(:user) { create(:user) }
+  let!(:warehouse) { create(:warehouse) }
 
   describe 'users#new' do
     it 'returns 200 status' do
@@ -13,5 +14,26 @@ describe UsersController, type: :controller do
       expect(response).to render_template(:new)
     end
   end
+
+  describe 'users#create' do
+    context 'successful user creation' do
+      before(:each) { post :create, params: {user: { name: "Reginald", employee_num: 777, password: "ham", warehouse: "Seattle"}, role: "scientist"} }
+      it 'redirects to parts_path' do
+        expect(response).to redirect_to parts_path
+      end
+    end
+
+    context 'unsuccessful user creation' do
+      it 'assigns "needs warehouse" error' do
+        post :create, params: {user: { name: "Reginald", employee_num: 777, password: "ham", warehouse: ""}, role: "scientist"}
+        expect(assigns[:errors]).to include "Warehouse must exist"
+      end
+      it 'assigns "user already exists" error' do
+        post :create, params: {user: { name: "Reginald", employee_num: user.employee_num, password: "ham", warehouse: "Seattle"}, role: "scientist"}
+        expect(assigns[:errors]).to include "Employee num has already been taken"
+      end
+    end
+  end
+
 
 end
