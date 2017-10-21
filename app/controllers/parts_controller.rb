13a971fc8a41197d90f698121a3096a513455f40
John Simoni & Marlo Ito <pair+jsimoni42.marloito@devbootcamp.com>
2017-10-20 19:43:01 -0700

    Adds part edit

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -2,21 +2,47 @@ class PartsController < ApplicationController
   def index
     if session[:user_id] == nil
       redirect_to new_session_path
+
     elsif is_manager?
       if params[:name]
         @warehouse = Warehouse.find_by(name: params[:name])
+
         if @warehouse == nil
           @errors = ["Warehouse doesn't exist"]
           @parts = Part.all
         else
           @parts = @warehouse.parts.uniq
         end
+
       else
         @parts = Part.all
       end
+
     else
       @warehouse = User.find(session[:user_id]).warehouse
       @parts = @warehouse.parts.uniq
     end
+  end
+
+  def edit
+    @part = Part.find(params[:id])
+  end
+
+  def update
+    @part = Part.find(params[:id])
+    @part.assign_attributes(part_params)
+
+    if @part.save
+      redirect_to edit_part_path(@part)
+    else
+      @errors = @part.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def part_params
+    params.require(:part).permit(:part_number, :max_quantity)
   end
 end
